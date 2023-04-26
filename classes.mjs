af5817ec916559a318f1cd9daf598f98682402c8
a.pushkin <a.v.pushkin@gmail.com>
2023-04-26 17:09:50 +0300

    add console.log

--- classes.mjs
+++ classes.mjs
@@ -17,6 +17,7 @@ export class Good {
 export class GoodsList {
     #goods;
 
+
     constructor(filter = '', sortPrice = false, sortDir = true) {
       this.#goods = [];
       this.filter = filter;
@@ -25,7 +26,7 @@ export class GoodsList {
     }
   
     get list() {
-      let filteredGoods = this.#goods.filter(good => good.available && good.name.match(this.filter));
+      let filteredGoods = this.#goods.filter(good => good.available && this.filter.test(good.name));
       if (this.sortPrice) {
         filteredGoods.sort((a, b) => {
           if (this.sortDir) {
@@ -47,5 +48,51 @@ export class GoodsList {
       if (index !== -1) {
         this.#goods.splice(index, 1);
       }
+    }
+  }
+
+  export class BasketGood extends Good {
+    constructor(good, amount) {
+      super(good.id, good.name, good.description, good.sizes, good.price, good.available);
+      this.amount = amount;
+    }
+  }
+
+  export class Basket {
+    #goods = [];
+  
+    get totalAmount() {
+      return this.#goods.reduce((total, good) => total + good.amount, 0);
+    }
+  
+    get totalSum() {
+      return this.#goods.reduce((total, good) => total + good.amount * good.price, 0);
+    }
+  
+    add(good, amount) {
+      const basketGood = this.#goods.find((basketGood) => basketGood.id === good.id);
+      if (basketGood) {
+        basketGood.amount += amount;
+      } else {
+        this.#goods.push(new BasketGood(good, amount));
+      }
+    }
+  
+    remove(good, amount) {
+      const basketGood = this.#goods.find((basketGood) => basketGood.id === good.id);
+      if (basketGood) {
+        basketGood.amount -= amount;
+        if (basketGood.amount <= 0) {
+          this.#goods.splice(this.#goods.indexOf(basketGood), 1);
+        }
+      }
+    }
+  
+    clear() {
+      this.#goods = [];
+    }
+  
+    removeUnavailable() {
+      this.#goods = this.#goods.filter((basketGood) => basketGood.available);
     }
   }
--- index.mjs
+++ index.mjs
@@ -1,14 +1,37 @@
-import {Good, GoodsList} from "./classes.mjs";
+import {Basket, Good, GoodsList} from "./classes.mjs";
 
-const good1 = new Good(1, 'T-shirt', 'Cotton T-shirt', ['S', 'M', 'L'], 20, true);
-const good2 = new Good(2, 'Hoodie', 'Cotton Hoodie', ['S', 'M', 'L'], 40, true);
-const good3 = new Good(3, 'Sweatpants', 'Cotton Sweatpants', ['S', 'M', 'L'], 30, false);
 const goodsList = new GoodsList(/t-shirt/i, true, false);
+const basket1 = new Basket();
+
+const good1 = new Good(1, 'T-shirt', 'Comfortable and stylish cotton t-shirt', ['s', 'l', 'm'], 100, true);
+const good2 = new Good(2, 'Sneakers', 'Sporty and fashionable sneakers', ['s', 'l', 'm'], 200, false);
+const good3 = new Good(3, 'Jeans', 'Stylish and durable denim jeans', ['s', 'l', 'm'], 150, true);
+const good4 = new Good(4, 'Backpack', 'Functional and stylish backpack for everyday use', ['s', 'l', 'm'], 80, false);
+const good5 = new Good(5, 'Watch', 'Elegant and reliable wristwatch', ['s', 'l', 'm'], 300, true);
+
 goodsList.add(good1);
 goodsList.add(good2);
 goodsList.add(good3);
+goodsList.add(good4);
+goodsList.add(good5);
 
 
-// console.log(goodsList.list)
-goodsList.filter = /hood/i
+basket1.add(good1, 2)
+basket1.add(good2, 2)
+
+console.log(`\nКорзина\nВсего: ${basket1.totalAmount} На сумму:${basket1.totalSum}`)
+
+basket1.remove(good1, 1)
+console.log(`\nКорзина после удаления одного товара\nВсего: ${basket1.totalAmount} На сумму:${basket1.totalSum}`)
+
+basket1.removeUnavailable()
+console.log(`\nКорзина после удаления недоступного товара\nВсего: ${basket1.totalAmount} На сумму:${basket1.totalSum}`)
+
+console.log(`\nВ каталоге по фильтру: ${goodsList.filter}`)
 console.log(goodsList.list)
+
+goodsList.filter = /jeans/i
+console.log(`\nВ каталоге по фильтру: ${goodsList.filter}`)
+console.log(goodsList.list)
+
+basket1.clear
